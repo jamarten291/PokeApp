62bd3ddf57051103148859a3e46d273ae9a36bbd
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2026-04-30 13:49:12 +0200

    feat(ui): Enhance PokemonListTile with sprite image and favorite button
    - Add a `leading` `CircleAvatar` to display the Pokemon's front default sprite.
    - Add a `trailing` `IconButton` with a favorite border icon.

--- lib/widgets/pokemon_list_tile.dart
+++ lib/widgets/pokemon_list_tile.dart
@@ -29,6 +29,14 @@ class PokemonListTile extends ConsumerWidget {
     return Skeletonizer(
       enabled: isLoading,
       child: ListTile(
+        // If the pokemon is not null, show the pokemon image.
+        leading: pokemon != null
+            ? CircleAvatar(
+                backgroundImage: NetworkImage(
+                  pokemon.sprites!.frontDefault!,
+                ),
+              )
+            : CircleAvatar(),
         title: Text(
           // If the pokemon is null, return an empty string.
           pokemon?.name!.toUpperCase()
@@ -39,6 +47,12 @@ class PokemonListTile extends ConsumerWidget {
         ),
         subtitle: Text(
           "Has ${pokemon?.moves?.length.toString() ?? '0'} moves."
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.favorite_border,
+          ),
         ),
       ),
     );
